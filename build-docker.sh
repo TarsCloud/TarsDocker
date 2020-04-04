e78f1e4e78b3cb81ad39d024819b388b69e9622c
ruanshudong <ruanshudong@qq.com>
2020-04-04 21:13:53 +0800

    add tarsnode/java nodejs cpp

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,6 @@
 
 if [ $# -lt 2 ]; then
-    echo "$0 COMMAND(base/tars/framework/tars-node/php) TAG"
+    echo "$0 COMMAND(base/framework/tars/tars-node/php/java/nodejs/cpp/go) TAG"
     exit 0
 fi
 
@@ -22,12 +22,28 @@ case $1 in
         ;;
     "tars-node")
         docker rmi -f tarscloud/tars-node:$TAG
-        docker build -t tarscloud/tars-node:$TAG tars-node
+        docker build -t tarscloud/tars-node:$TAG tars-node/full
         ;;
     "php")
-        docker rmi -f tars-node/tars-node/tars-node-php:$TAG
-        docker build -t tarscloud/tars-node/tars-node-php:$TAG tars-node/php
+        docker rmi -f tarscloud/tars-node-php:$TAG
+        docker build -t tarscloud/tars-node-php:$TAG tars-node/php
+        ;;
+    "java")
+        docker rmi -f tarscloud/tars-node-java:$TAG
+        docker build -t tarscloud/tars-node-java:$TAG tars-node/java
+        ;;
+    "nodejs")
+        docker rmi -f tarscloud/tars-node-nodejs:$TAG
+        docker build -t tarscloud/tars-node-nodejs:$TAG tars-node/nodejs
+        ;;
+    "go")
+        docker rmi -f tarscloud/tars-node-go:$TAG
+        docker build -t tarscloud/tars-node-go:$TAG tars-node/cpp
+        ;;
+    "cpp")
+        docker rmi -f tarscloud/tars-node-cpp:$TAG
+        docker build -t tarscloud/tars-node-cpp:$TAG tars-node/cpp
         ;;
    *)
-        echo "[$0 COMMAND TAG]: COMMAND: base/tars/framework/tars-node/php"
+        echo "[$0 COMMAND TAG]: COMMAND: base/tars/framework/tars-node/php/java/nodejs/cpp/go"
 esac
