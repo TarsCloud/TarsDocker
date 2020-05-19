42bde595e709106f2ca570353515bb010c08f484
ruanshudong <ruanshudong@qq.com>
2020-05-19 22:42:42 +0800

    update tars-chart

--- build-docker.sh
+++ build-docker.sh
@@ -22,27 +22,27 @@ case $1 in
         ;;
     "tars-node")
         docker rmi -f tarscloud/tars-node:$TAG
-        docker build -t tarscloud/tars-node:$TAG tars-node/full
+        docker build -t tarscloud/tars-node:$TAG tarsnode/full
         ;;
     "php")
         docker rmi -f tarscloud/tars-node-php:$TAG
-        docker build -t tarscloud/tars-node-php:$TAG tars-node/php
+        docker build -t tarscloud/tars-node-php:$TAG tarsnode/php
         ;;
     "java")
         docker rmi -f tarscloud/tars-node-java:$TAG
-        docker build -t tarscloud/tars-node-java:$TAG tars-node/java
+        docker build -t tarscloud/tars-node-java:$TAG tarsnode/java
         ;;
     "nodejs")
         docker rmi -f tarscloud/tars-node-nodejs:$TAG
-        docker build -t tarscloud/tars-node-nodejs:$TAG tars-node/nodejs
+        docker build -t tarscloud/tars-node-nodejs:$TAG tarsnode/nodejs
         ;;
     "go")
         docker rmi -f tarscloud/tars-node-go:$TAG
-        docker build -t tarscloud/tars-node-go:$TAG tars-node/cpp
+        docker build -t tarscloud/tars-node-go:$TAG tarsnode/cpp
         ;;
     "cpp")
         docker rmi -f tarscloud/tars-node-cpp:$TAG
-        docker build -t tarscloud/tars-node-cpp:$TAG tars-node/cpp
+        docker build -t tarscloud/tars-node-cpp:$TAG tarsnode/cpp
         ;;
    *)
         echo "[$0 COMMAND TAG]: COMMAND: base/tars/framework/tars-node/php/java/nodejs/cpp/go"
