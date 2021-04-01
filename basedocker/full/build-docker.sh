f73114418efc9bed89c7aa87a3533dac20896d57
ruanshudong <ruanshudong@qq.com>
2021-04-01 11:32:40 +0800

    fix build-docker in basedocker bug

--- basedocker/full/build-docker.sh
+++ basedocker/full/build-docker.sh
@@ -9,11 +9,9 @@ fi
 
 dockerTag=$1
 
-WORKING_DIR=$(cd $(dirname "$0") && pwd)
-
 export DOCKER_CLI_EXPERIMENTAL=enabled 
 docker buildx create --use --name tars-builder 
 docker buildx inspect tars-builder --bootstrap
 docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
 
-docker buildx build ${WORKING_DIR}  -t tarscloud/tars-env-full:$dockerTag --platform=linux/amd64,linux/arm64 -f basedocker/full/Dockerfile --push
+docker buildx build . -t tarscloud/tars-env-full:$dockerTag --platform=linux/amd64,linux/arm64 -f Dockerfile --push
