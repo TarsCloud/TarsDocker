2e9fc04ad8f018cee3f39add9d8890e691d4c010
ruanshudong <ruanshudong@qq.com>
2021-04-02 18:27:19 +0800

    add tars-node build

---
+++ tarsnode/build-docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if (( $# < 1 ))
+then
+    echo $#
+    echo "$0 dir(cpp/full/java/nodejs/php)"
+    exit 1
+fi
+
+dir=$1
+
+dockerTag=$2
+
+if [ "$dockerTag" == "" ]; then
+    dockerTag=$dir
+fi
+
+if [ ! -d $dir ]; then
+    echo "dir:$dir not exists."
+    exit -1
+fi
+
+export DOCKER_CLI_EXPERIMENTAL=enabled 
+docker buildx create --use --name tars-builder 
+docker buildx inspect tars-builder --bootstrap
+docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
+
+docker buildx build $1 -t tarscloud/tars-node:$dockerTag --platform=linux/amd64,linux/arm64 -f $1/Dockerfile --push
