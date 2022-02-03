92037c49e96e947ec74593126c2b5ac14607e334
lbbniu <lbbniu@gmail.com>
2022-02-03 09:32:09 +0800

    适配新版本k8s 和 helm

--- tars-chart/dev/tars-install.sh
+++ tars-chart/dev/tars-install.sh
@@ -7,7 +7,7 @@ echo $NS, $user
 if [ "$NS" == "" ]; then
     echo "create tars-k8s"
     kubectl create namespace $user
-    helm install tars --name $user  --namespace $user --set tarsnode.replicas=2,tars.namespace=$user
+    helm install $user tars  --namespace $user --set tarsnode.replicas=1,tars.namespace=$user
 else
     echo "upgrade tars-k8s"
     helm upgrade $user tars
--- tars-chart/tars-install.sh
+++ tars-chart/tars-install.sh
@@ -7,9 +7,9 @@ echo $NS, $user
 if [ "$NS" == "" ]; then
     echo "create tars-k8s"
     kubectl create namespace $user
-    helm install tars-stable --name $user  --namespace $user --set tarsnode.replicas=2,tars.namespace=$user
+    helm install $user ./stable/tars --namespace $user --set tarsnode.replicas=1,tars.namespace=$user
 else
     echo "upgrade tars-k8s"
-    helm upgrade $user tars-stable
+    helm upgrade $user ./stable/tars
 
 fi
