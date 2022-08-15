11e1ac7aa0e05a128bf6539efdd57232565737a1
ruanshudong <ruanshudong@qq.com>
2022-08-15 16:41:33 +0800

    fix qmemu

--- compiler/build-docker.sh
+++ compiler/build-docker.sh
@@ -9,6 +9,7 @@ fi
 export DOCKER_CLI_EXPERIMENTAL=enabled 
 docker buildx create --use --name tars-builder-compiler
 docker buildx inspect tars-builder-compiler --bootstrap
-docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
+docker run --rm --privileged tonistiigi/binfmt:latest --install all
+
 
 docker buildx build . -t tarscloud/compiler:$dockerTag --platform=linux/amd64,linux/arm64 -f Dockerfile --push
